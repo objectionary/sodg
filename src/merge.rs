df203f4c56a365607749ee8b88d24c411612b0c9
Yegor Bugayenko <yegor256@gmail.com>
2023-02-16 20:48:29 +0800

    #65 simple test works

--- src/merge.rs
+++ src/merge.rs
@@ -25,22 +25,67 @@ use std::collections::HashMap;
 impl Sodg {
     /// Merge another graph into itself.
     pub fn merge(&mut self, g: &Sodg) {
-        let mut matcher: HashMap<u32, u32> = HashMap::new();
-        let mut next = self.next_id();
+        let mut ups: HashMap<u32, Vec<(u32, String)>> = HashMap::new();
         for (v, vtx) in g.vertices.iter() {
-            let mut id = 0;
-            if *v != 0 {
-                id = next;
-                next += 1;
+            if !ups.contains_key(v) {
+                ups.insert(*v, vec![]);
             }
-            matcher.insert(*v, id);
-            self.vertices.insert(id, vtx.clone());
+            for e in vtx.edges.iter() {
+                if !ups.contains_key(&e.to) {
+                    ups.insert(e.to, vec![]);
+                }
+                ups.get_mut(&e.to).unwrap().push((*v, e.a.clone()));
+            }
         }
-        for v in matcher.values() {
-            let vtx = self.vertices.get_mut(v).unwrap();
-            for e in vtx.edges.iter_mut() {
-                e.to = *matcher.get(v).unwrap();
+        debug!("ups: {ups:?}");
+        let mut rename: HashMap<u32, u32> = HashMap::new();
+        rename.insert(0, 0);
+        loop {
+            for (v, _vtx) in g.vertices.iter() {
+                if rename.contains_key(v) {
+                    continue;
+                }
+                let mut found = None;
+                for up in ups.get(v).unwrap() {
+                    let left = rename.get(&up.0);
+                    if left.is_none() {
+                        continue;
+                    }
+                    let has = match self.vertices.get(left.unwrap()) {
+                        None => Vertex::empty(),
+                        Some(vt) => vt.clone(),
+                    };
+                    for h in has.edges {
+                        if h.a == up.1 {
+                            if found.is_some() && found.unwrap() != h.to {
+                                panic!("Double parents");
+                            }
+                            found = Some(h.to);
+                        }
+                    }
+                }
+                if found.is_none() {
+                    found = Some(self.next_id());
+                }
+                rename.insert(*v, found.unwrap());
             }
+            if rename.len() >= g.vertices.len() {
+                break;
+            }
+        }
+        debug!("rename: {rename:?}");
+        for (v, vtx) in g.vertices.iter() {
+            let new = rename.get(v).unwrap();
+            let mut before = match self.vertices.get(new) {
+                None => Vertex::empty(),
+                Some(b) => b.clone(),
+            };
+            for e in vtx.edges.iter() {
+                let v1 = *rename.get(&e.to).unwrap();
+                before.edges.retain(|e1| &e1.a != e.a.as_str());
+                before.edges.push(Edge::new(v1, e.a.as_str()));
+            }
+            self.vertices.insert(*new, before);
         }
         debug!(
             "Merged {} vertices into the existing Sodg",
@@ -49,6 +94,8 @@ impl Sodg {
     }
 }
 
+use crate::edge::Edge;
+use crate::vertex::Vertex;
 #[cfg(test)]
 use anyhow::Result;
 
@@ -64,5 +111,24 @@ fn merges_two_graphs() -> Result<()> {
     extra.bind(0, 1, "bar")?;
     g.merge(&extra);
     assert_eq!(3, g.vertices.len());
+    Ok(())
+}
+
+#[test]
+fn avoids_simple_duplicates() -> Result<()> {
+    let mut g = Sodg::empty();
+    g.add(0)?;
+    g.add(1)?;
+    g.bind(0, 1, "foo")?;
+    let mut extra = Sodg::empty();
+    extra.add(0)?;
+    extra.add(1)?;
+    extra.bind(0, 1, "foo")?;
+    extra.add(2)?;
+    extra.bind(1, 2, "bar")?;
+    g.merge(&extra);
+    debug!("{g:?}");
+    assert_eq!(3, g.vertices.len());
+    assert_eq!(1, g.kid(0, "foo").unwrap().0);
     Ok(())
 }
